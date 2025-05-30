97695743e3b37b73338c2092cc56e71fdaa4509c
pirawinkumar <155950662+pirawinkumar@users.noreply.github.com>
2025-05-30 09:22:35 +0530

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -11,9 +11,16 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 builder.Services.AddOpenApi();
 
+
+
 var app = builder.Build();
+app.UseCors(opt =>
+{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:3000");
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
